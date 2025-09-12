eecc3584246be16154328ecdf20f3d713e720012
znak99 <xhahekcldhkznak0113@gmail.com>
2025-09-12 21:13:32 +0900

    Setup directory structure

---
+++ GithubSearch/GithubSearch/Config/AppConfig.swift
@@ -0,0 +1,8 @@
+//
+//  AppConfig.swift
+//  GithubSearch
+//
+//  Created by seungwoo on 2025/09/12.
+//
+
+import Foundation
---
+++ GithubSearch/GithubSearch/View/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  GithubSearch
+//
+//  Created by seungwoo on 2025/09/12.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootView()
+}
