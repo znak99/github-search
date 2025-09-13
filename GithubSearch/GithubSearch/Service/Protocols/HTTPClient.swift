45dae3a2e106cb31dc978030929db523659a499c
znak99 <xhahekcldhkznak0113@gmail.com>
2025-09-13 17:03:06 +0900

    Delete Network directory
    Seperate HTTPClient file

--- GithubSearch/GithubSearch/Service/Protocols/HTTPClient.swift
+++ GithubSearch/GithubSearch/Service/Protocols/HTTPClient.swift
@@ -8,14 +8,8 @@
 import Foundation
 
 /// GitHub API通信用のHTTPクライアント
-/// - 生成されたリクエストを実行
 public protocol HTTPClient: Sendable {
     func data(for request: URLRequest) async throws -> (Data, URLResponse)
 }
 
-public struct GitHubHTTPClient: HTTPClient {
-    public init() {}
-    public func data(for request: URLRequest) async throws -> (Data, URLResponse) {
-        try await URLSession.githubDefault.data(for: request)
-    }
-}
+
