30f9a217e94e2ee8d96133d1c64a87486b0e5612
znak99 <xhahekcldhkznak0113@gmail.com>
2025-09-14 23:36:20 +0900

    Update count of RepositorySkeleton 1 → 2

--- GithubSearch/GithubSearch/Features/SearchRepository/View/Components/RepositorySkeleton.swift
+++ GithubSearch/GithubSearch/Features/SearchRepository/View/Components/RepositorySkeleton.swift
@@ -10,42 +10,44 @@ import Shimmer
 
 struct RepositorySkeleton: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            HStack {
-                ZStack {
-                    Circle().fill(.secondary)
-                    Image("github-avatar")
-                        .resizable()
-                        .scaledToFit()
-                }
-                .squareFrame(40, range: 4)
-                
+        VStack {
+            ForEach(0..<2) { _ in
                 VStack(alignment: .leading) {
+                    HStack {
+                        ZStack {
+                            Circle().fill(.secondary)
+                            Image("github-avatar")
+                                .resizable()
+                                .scaledToFit()
+                        }
+                        .squareFrame(40, range: 4)
+                        
+                        VStack(alignment: .leading) {
+                            RoundedRectangle(cornerRadius: 4)
+                                .fill(.secondary)
+                                .frame(width: 50, height: 12)
+                            RoundedRectangle(cornerRadius: 4)
+                                .fill(.secondary)
+                                .frame(width: 100, height: 12)
+                        }
+                        Spacer()
+                    }
                     RoundedRectangle(cornerRadius: 4)
                         .fill(.secondary)
-                        .frame(width: 50, height: 12)
+                        .frame(width: 250, height: 12)
                     RoundedRectangle(cornerRadius: 4)
                         .fill(.secondary)
-                        .frame(width: 100, height: 12)
+                        .frame(width: 200, height: 12)
                 }
-                Spacer()
+                .padding(8)
+                .background {
+                    RoundedRectangle(cornerRadius: 8)
+                        .fill(.surface)
+                }
+                .shimmering()
             }
-            RoundedRectangle(cornerRadius: 4)
-                .fill(.secondary)
-                .frame(width: 250, height: 12)
-            RoundedRectangle(cornerRadius: 4)
-                .fill(.secondary)
-                .frame(width: 200, height: 12)
+            
+            Spacer()
         }
-        .padding(8)
-        .background {
-            RoundedRectangle(cornerRadius: 8)
-                .fill(.surface)
-        }
-        .shimmering()
     }
-}
-
-#Preview {
-    RepositorySkeleton()
 }
